dc6ee496c4f25fbb387969781dc9dae7c8fb7397
Alexandre10y <alexandre10y@outlook.com>
2024-02-16 10:12:14 -0300

    upload tree

---
+++ Pages/trabalho/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona a classe 'hidden' após a animação de pulsação
+    setTimeout(function() {
+        document.querySelectorAll('.falling-letters span').forEach(function(letter) {
+            letter.classList.add('hidden');
+        });
+
+        // Redireciona para a nova página após 2 segundos (tempo da animação de pulsação)
+        setTimeout(function() {
+            window.location.href = "../../index.html"; // Substitua "nova_pagina.html" pelo nome da sua nova página
+        }, 2000);
+
+    }, 5000); // 2s para a animação de pulsação + 2s de atraso
+});
